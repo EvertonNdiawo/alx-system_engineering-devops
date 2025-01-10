235f11548adcf87219211319b7a96c434d8118cb
EvertonNdiawo <ndiawofreelancer@gmail.com>
2025-01-10 23:01:53 +0300

    More additions

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# A puppet manifest to installs the package 'flask -version 2.1.0' from pip3
-
+#install a flask from pip3
 exec { 'install flask':
   command => 'pip3 install flask==2.1.0',
   path    => '/usr/bin',
