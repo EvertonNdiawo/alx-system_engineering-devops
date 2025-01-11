2dfe94617549d00e00d835b702fde3ed2c91f792
EvertonNdiawo <ndiawofreelancer@gmail.com>
2025-01-11 12:43:19 +0300

    Crude but effective way to solve the task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #install flask 2.1.0 using pip3
-package { 'flask':
-  ensure   =>  '2.1.0',
-  provider =>  'pip3',
+exec { 'install flask with dependencies':
+  command => 'pip3 install flask==2.1.0 werkzeug==2..3',
+  path    => ['/usr/bin', '/bin'],
+  unless  => 'pip3 list | grep flask | grep 2.1.0',
 }
